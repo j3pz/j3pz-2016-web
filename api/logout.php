07ff7afd500b18f0a42b0b893f4e08a12f9af957
ziofat <printlts@gmail.com>
2016-02-24 09:39:00 +1100

    ignore

--- api/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-	setcookie("user", "", time()-3600,"/");
-	setcookie('name', "", time()-3600,"/");
-	setcookie('uid', "", time()-3600,"/");
-	setcookie('token', "", time()-3600,"/");
-	// header("Location:/");
-?>
