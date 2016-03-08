533d0ef5695cc3120fcf9ca7ad30beab58b7b55f
ziofat <printlts@gmail.com>
2016-03-09 01:15:04 +1100

    fix(weibo):修复微博登录问题

--- components/modals/login.js
+++ components/modals/login.js
@@ -1,5 +1,5 @@
 /* @require /components/config/config.js */
-app.controller('LoginCtrl', ['$rootScope','$scope','$httpParamSerializerJQLike','toastr','$http','$interval', function($rootScope,$scope,$httpParamSerializerJQLike,toastr,$htt,$interval){
+app.controller('LoginCtrl', ['$rootScope','$scope','$httpParamSerializerJQLike','toastr','$http','$interval', function($rootScope,$scope,$httpParamSerializerJQLike,toastr,$http,$interval){
 	$scope.login = function(){
 		$rootScope.user.isLoading = true;
 		$http({
@@ -14,22 +14,7 @@ app.controller('LoginCtrl', ['$rootScope','$scope','$httpParamSerializerJQLike',
 			if(response.err||!response.isLogin){
 				toastr.error("登录失败, " + response.errReason);
 			}else{
-				$rootScope.isLogin = true;
-				$rootScope.user.name = response.name;
-				if($rootScope.isPz){
-					$("#quality-range").slider({range: true, min: 450, max: 1100, values: $rootScope.equipListfilter.range, step:5,
-						slide: function (event, ui) {
-							$rootScope.equipListfilter.range[0] = ui.values[0];
-							$rootScope.equipListfilter.range[1] = ui.values[1];
-							$rootScope.$apply();
-						}
-					});
-					$rootScope.equipListfilter.range = response.preference.quality;
-					$rootScope.embedLevel = response.preference.magicStoneLevel;
-					$rootScope.strengthenLevel = response.preference.strengthen;
-					$rootScope.saveList.isLoad = false;
-					$scope.$emit("saveCase");
-				}
+				$scope.loginSuccess(response);
 			}
 		})
 		.error(function(response) {
@@ -39,34 +24,44 @@ app.controller('LoginCtrl', ['$rootScope','$scope','$httpParamSerializerJQLike',
 			$("#loginModal").modal('hide');
 		});
 	};
+
+	$scope.loginSuccess = function(response){
+		$rootScope.isLogin = true;
+		$rootScope.user.name = response.name;
+		if($rootScope.isPz){
+			$("#quality-range").slider({range: true, min: 450, max: 1100, values: $rootScope.equipListfilter.range, step:5,
+				slide: function (event, ui) {
+					$rootScope.equipListfilter.range[0] = ui.values[0];
+					$rootScope.equipListfilter.range[1] = ui.values[1];
+					$rootScope.$apply();
+				}
+			});
+			$rootScope.equipListfilter.range = response.preference.quality;
+			$rootScope.embedLevel = response.preference.magicStoneLevel;
+			$rootScope.strengthenLevel = response.preference.strengthen;
+			$rootScope.saveList.isLoad = false;
+			$scope.$emit("saveCase");
+		}
+	}
 	$scope.weiboLogin = function(){
 		WB2.login(function() {
-			console.log(1);
+			$('#loginModal').modal('hide');
+			$scope.weiboOauthData = angular.copy(WB2.oauthData);
+			$interval.cancel($scope.weiboStatusReadyCheck);
+			if(WB2.checkLogin()){
+				$http.get(config.apiBase+'weiboBind.php?type=login')
+				.success(function(response){
+					if(response.isLogin){
+						$scope.loginSuccess(response);
+					}else if(response.needBind){
+						// 打开绑定页面
+					}
+				})
+				.error(function(response) {
+					toastr.error("授权失败或错误");
+				});
+			}
 		});
 	};
 	$scope.weiboOauthData = {};
-	angular.element(document).ready(function () {
-		$scope.weiboStatusReadyCheck = $interval(function(){
-			if(!!WB2.oauthData){
-				$scope.weiboOauthData = angular.copy(WB2.oauthData);
-				$interval.cancel($scope.weiboStatusReadyCheck);
-				if(WB2.checkLogin()){
-					WB2.anyWhere(function (W) {
-						W.parseCMD("/users/show.json", function (oResult, bStatus) {
-							if (!!bStatus) {
-								console.log(oResult);
-							}else{
-								alert("授权失败或错误");
-							}
-						}, {
-							uid : $scope.weiboOauthData.uid
-						}, {
-							method: 'GET',
-							cache_time: 0
-						});
-					});
-				}
-			}
-		},1000);
-	});
 }]);
