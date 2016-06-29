b6f289a34b59e67d52903161e17881283fdc2a78
ziofat <printlts@gmail.com>
2016-06-30 02:17:31 +1000

    修改：仍然使用 bootstrap-slider 作为滑块插件，因为它支持触屏

--- pages/build.js
+++ pages/build.js
@@ -1,4 +1,4 @@
-var app = angular.module('J3pz', ['toastr','ui.select','ngDraggable','angular-loading-bar','cfp.hotkeys','ui.bootstrap']);
+var app = angular.module('J3pz', ['toastr','ui.select','ngDraggable','angular-loading-bar','cfp.hotkeys','ui.bootstrap','ui.bootstrap-slider']);
 app.config(['toastrConfig',function(toastrConfig) {
 	angular.extend(toastrConfig, {
 		maxOpened: 5,
