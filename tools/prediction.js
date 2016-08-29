8f327c29ac2b88f2a39d11b6b6b2b123b80c6e9d
ziofat <printlts@gmail.com>
2016-08-29 15:36:24 +1000

    style: 修改代码格式

--- tools/prediction.js
+++ tools/prediction.js
@@ -3,8 +3,8 @@
    @require /components/equip/equip.js
 */
 
-var app = angular.module('J3Prediction', ['ui.bootstrap','toastr'])
-.config(['toastrConfig',function(toastrConfig) {
+var app = angular.module('J3Prediction', ['ui.bootstrap', 'toastr'])
+.config(['toastrConfig', function(toastrConfig) {
 	angular.extend(toastrConfig, {
 		maxOpened: 5,
 		positionClass: 'toast-bottom-right',
@@ -12,82 +12,82 @@ var app = angular.module('J3Prediction', ['ui.bootstrap','toastr'])
 	});
 }]);
 
-app.controller('GuishiCtrl', ['$scope','$http','toastr', function($scope,$http,toastr){
+app.controller('GuishiCtrl', ['$scope', '$http', 'toastr', function($scope, $http, toastr) {
 	$scope.data = {};
 	$scope.currentInstance = {};
 	$scope.currentBoss = {};
-	$scope.getPredictionData = function(){
-		$http.get("/data/prediction.json")
-		.success(function(response){
+	$scope.getPredictionData = function() {
+		$http.get('/data/prediction.json')
+		.success(function(response) {
 			$scope.data = response;
 			$scope.chooseInstance(0);
 		});
-	}
+	};
 
 	$scope.getPredictionData();
 
-	$scope.chooseInstance = function(id){
+	$scope.chooseInstance = function(id) {
 		$scope.currentInstance = $scope.data[id];
 		$scope.chooseBoss(0);
-	}
+	};
 
-	$scope.updateStone = function(){
+	$scope.updateStone = function() {
 		$scope.stoneList = [];
 		for (var i = 0; i < $scope.currentInstance.prediction.length; i++) {
-			if($scope.currentInstance.prediction[i].boss==$scope.currentBoss){
+			if ($scope.currentInstance.prediction[i].boss == $scope.currentBoss) {
 				$scope.stoneList.push($scope.currentInstance.prediction[i]);
 			}
 		}
-	}
+	};
 
-	$scope.chooseBoss = function(id){
+	$scope.chooseBoss = function(id) {
 		$scope.currentBoss = $scope.currentInstance.boss[id];
-		$scope.updateStone()
-	}
+		$scope.updateStone();
+	};
 
-	$scope.allInfo = ["texiao","xiangqian"];
-	$scope.basicInfoWithoutJL = ["basicPhysicsShield","basicMagicShield"];
-	$scope.basicInfoWithJL = ["body","spirit","spunk","strength","agility"];
-	$scope.plusInfo = ["physicsShield","magicShield","dodge","parryBase","parryValue","attack","heal","crit","critEffect","overcome","toughness","acce","hit","strain","huajing","threat"];
-	var positions = ["项链","腰坠","戒指","戒指","暗器","鞋子","护腕","下装","上衣","帽子","腰带","武器"];
+	$scope.allInfo = ['texiao', 'xiangqian'];
+	$scope.basicInfoWithoutJL = ['basicPhysicsShield', 'basicMagicShield'];
+	$scope.basicInfoWithJL = ['body', 'spirit', 'spunk', 'strength', 'agility'];
+	$scope.plusInfo = ['physicsShield', 'magicShield', 'dodge', 'parryBase', 'parryValue', 'attack', 'heal', 'crit', 'critEffect', 'overcome', 'toughness', 'acce', 'hit', 'strain', 'huajing', 'threat'];
+	var positions = ['项链', '腰坠', '戒指', '戒指', '暗器', '鞋子', '护腕', '下装', '上衣', '帽子', '腰带', '武器'];
 
-	$scope.searchPreview = function(name){
+	$scope.searchPreview = function(name) {
 		$scope.searchEquipPreview = Equip.createNew();
-		nameArr = name.split("[");
-		nameArr = nameArr[1].split("]");
-		name = nameArr[0];
-		if(name.indexOf("儒风")>=0) return;
-		var url = config.apiBase+'equip/'+name;
+		var nameArr = name.split('[');
+		nameArr = nameArr[1].split(']');
+		var rname = nameArr[0];
+		if (rname.indexOf('儒风') >= 0) return;
+		var url = config.apiBase + 'equip/' + rname;
 		url = encodeURI(url);
 		$http.get(url)
-		.success(function(response){
-			if(response.errors) {
-				toastr.error("载入装备失败，"+response.errors[0]);
-			}else{
+		.success(function(response) {
+			if (response.errors) {
+				toastr.error('载入装备失败，' + response.errors[0]);
+			} else {
 				response = response.data;
 				var equipXinfaType = response.xinfatype;
 				var preDesc = analysisPre(equipXinfaType);
 				$scope.searchEquipPreview.type = response.type;
 				$scope.searchEquipPreview.typeParse = positions[response.type];
-				preDescAvaList["attack"]= preDesc[0]+"攻击提高";
-				preDescAvaList["heal"]= preDesc[0]+"治疗成效提高";
-				preDescAvaList["crit"]= preDesc[1]+"会心等级提高";
-				preDescAvaList["critEffect"]= preDesc[1]+"会心效果提高";
-				preDescAvaList["overcome"]= preDesc[0]+"破防等级提高";
-				preDescAvaList["hit"]= preDesc[1]+"命中等级提高";
-				angular.forEach(response, function(value,key){
-					if(value!=="0"||key=="type"){
-						this.setData(key,value);
+				preDescAvaList['attack'] = preDesc[0] + '攻击提高';
+				preDescAvaList['heal'] = preDesc[0] + '治疗成效提高';
+				preDescAvaList['crit'] = preDesc[1] + '会心等级提高';
+				preDescAvaList['critEffect'] = preDesc[1] + '会心效果提高';
+				preDescAvaList['overcome'] = preDesc[0] + '破防等级提高';
+				preDescAvaList['hit'] = preDesc[1] + '命中等级提高';
+				angular.forEach(response, function(value, key) {
+					if (value !== '0' || key == 'type') {
+						this.setData(key, value);
 					}
-					if(key in preDescAvaList) this.setDesc(key,preDescAvaList[key]);
-				} ,$scope.searchEquipPreview);
+					if (key in preDescAvaList) this.setDesc(key, preDescAvaList[key]);
+				}, $scope.searchEquipPreview);
 				$scope.searchEquipPreview.setStrengthen(0);
 				$scope.searchEquipPreview.analysisHole();
 				$scope.searchEquipPreview.recommendSchool = preDesc[2];
 			}
 		})
-		.error(function(){
-			toastr.error("载入装备失败,请重试");
+		.error(function() {
+			toastr.error('载入装备失败,请重试');
 		});
-	}
+	};
 }]);
