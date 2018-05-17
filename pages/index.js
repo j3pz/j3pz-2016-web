8775c798495f01c2ef335f023a2e3b8325fb91e5
胖叔叔 <printlts@gmail.com>
2018-05-17 22:05:05 +0800

    新增广告

--- pages/index.js
+++ pages/index.js
@@ -6,3 +6,14 @@ var app = angular.module('J3pz', ['toastr'])
 		timeOut: 3000
 	});
 }]);
+
+$(document).ready(function() {
+  $('#custom-ad-0').click(function() {
+    console.log(0);
+    ga('send', 'event', 'Ad', 'click', 'banner', 1);
+  });
+  $('#custom-ad-1').click(function() {
+    console.log(1);
+    ga('send', 'event', 'Ad', 'click', 'link', 1);
+  });
+});
