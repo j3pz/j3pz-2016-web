3d881523a466493bc3e29962d68accd93a5faf56
胖叔叔 <printlts@gmail.com>
2018-06-21 01:18:15 +0800

    更新装备品级上限

--- pages/user.js
+++ pages/user.js
@@ -152,7 +152,7 @@ app.controller('UserCtrl', ['$scope', '$rootScope', '$http', 'toastr', '$locatio
 			var qualitySlider = $('input.slider-input').slider({
 				range: true,
 				min: 450,
-				max: 1400,
+				max: 1550,
 				values: $scope.preference.range,
 				step: 5,
 				tooltip: 'hide'
