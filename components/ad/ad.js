ba114f1ab130c098e3aa1ade279e1e9046820b77
胖叔叔 <printlts@gmail.com>
2018-07-25 23:53:42 +0800

    更新广告几率

--- components/ad/ad.js
+++ components/ad/ad.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
-  $('#custom-ad-1').click(function() {
-    console.log(2);
-    ga('send', 'event', 'Ad', 'click', 'building', 1);
-  });
+  var roll = Math.random();
+  if (roll < 50) {
+    $('#google-ad').hide();
+    $('#custom-ad-1').show();
+    $('#custom-ad-1').click(function() {
+      ga('send', 'event', 'Ad', 'click', 'building', 1);
+    });
+  }
 });
