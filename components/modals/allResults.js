4f9eb47cb97b6d478372653a734874de79afccf5
胖叔叔 <printlts@gmail.com>
2020-01-17 14:17:53 +0800

    增加复制属性功能

--- components/modals/allResults.js
+++ components/modals/allResults.js
@@ -1,4 +1,4 @@
-app.controller('ResultsShowController', ['$scope', '$rootScope', function($scope, $rootScope) {
+app.controller('ResultsShowController', ['$scope', '$rootScope', 'toastr', function($scope, $rootScope, toastr) {
 	$scope.result = $rootScope.results;
 	$scope.descList = {
 		body: '体质',
@@ -16,9 +16,11 @@ app.controller('ResultsShowController', ['$scope', '$rootScope', function($scope
 		heal: '治疗',
 		crit: '会心',
 		critEffect: '会效',
+		overcome: '破防',
 		overcomeRate: '破防',
 		hit: '命中',
 		acce: '加速',
+		acceLevel: '加速',
 		strain: '无双',
 		huajing: '化劲',
 		threat: '威胁',
@@ -30,4 +32,48 @@ app.controller('ResultsShowController', ['$scope', '$rootScope', function($scope
 	$scope.list_1 = ['life', 'body', 'spirit', 'spunk', 'strength', 'agility', 'basicAttack', 'attack'];
 	$scope.list_2 = ['crit', 'critEffect', 'hit', 'acce', 'overcomeRate', 'strain', 'basicHeal', 'heal'];
 	$scope.list_3 = ['physicsShield', 'magicShield', 'dodge', 'parryBase', 'parryValue', 'toughness', 'huajing', 'score'];
+
+	$scope.copyResult = function(format) {
+		var keys = $scope.list_1.concat($scope.list_2).concat($scope.list_3).map(function(key) {
+			if (key == 'acce') return 'acceLevel';
+			if (key == 'overcomeRate') return 'overcome';
+			return key;
+		});
+		var values = keys.map(function(key) {
+			return $scope.result[key] || 0;
+		});
+		var result = '';
+		if (format == 'csv') {
+			result += keys.map(function(key) {
+				return $scope.descList[key];
+			}).join(',');
+			result += '\n';
+			result += values.join(',');
+		} else if (format == 'json') {
+			var resultObj = keys.map(function(key) {
+				return [key, $scope.descList[key]];
+			}).reduce(function(acc, cur) {
+				var key = cur[0];
+				var desc = cur[1];
+				acc[desc] = '' + ($scope.result[key] || 0);
+				return acc;
+			}, {});
+			result = JSON.stringify(resultObj);
+		} else {
+			toastr.error('不支持的格式');
+		}
+		var copyzone = document.getElementById('copyzone');
+		copyzone.innerText = result;
+		var range = document.createRange();
+		range.selectNodeContents(copyzone);
+		var selection = window.getSelection();
+		selection.removeAllRanges();
+		selection.addRange(range);
+		try {
+			document.execCommand('copy', false, null);
+			toastr.info('复制成功');
+		} catch (e) {
+			toastr.info('复制失败');
+		}
+	}
 }]);
